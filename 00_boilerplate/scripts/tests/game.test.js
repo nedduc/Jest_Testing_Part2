2670a1343a19f0102b0793a94e62182279270c0d
nedduc <marinacudden@gmail.com>
2021-10-28 17:13:16 +0000

    Add array for choices, test

--- 00_boilerplate/scripts/tests/game.test.js
+++ 00_boilerplate/scripts/tests/game.test.js
@@ -24,5 +24,7 @@ describe("game object contains correct keys", () => {
     test("choices exists", () => {
         expect("choices" in game).toBe(true);
     });
-
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
