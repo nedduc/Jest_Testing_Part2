72bfd4e72f2fef91b011bc7225747a9f2551c9ff
nedduc <marinacudden@gmail.com>
2021-10-28 18:36:56 +0000

    Add beforeEach, afterEach & addTurn

--- 00_boilerplate/scripts/tests/game.test.js
+++ 00_boilerplate/scripts/tests/game.test.js
@@ -51,4 +51,24 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
-});     
+}); 
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = o;
+        game.currentGame = [];
+        game. playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = o;
+        game.currentGame = [];
+        game. playerMoves = [];
+        addTurn();
+
+    });
+    test("addTurn adds a new turn to the game", ()=> {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    })
+});
