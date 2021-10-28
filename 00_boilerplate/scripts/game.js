d9f9ed92eda4c6e2e3ce15da8f811fbce633fdcd
nedduc <marinacudden@gmail.com>
2021-10-28 17:18:40 +0000

    test for Test for new Game, fails, write new game function

--- 00_boilerplate/scripts/game.js
+++ 00_boilerplate/scripts/game.js
@@ -5,4 +5,5 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 }
 
+
 module.exports = {game};
--- 00_boilerplate/scripts/tests/game.test.js
+++ 00_boilerplate/scripts/tests/game.test.js
@@ -28,3 +28,12 @@ describe("game object contains correct keys", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
+desscribe("newGames works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+});
